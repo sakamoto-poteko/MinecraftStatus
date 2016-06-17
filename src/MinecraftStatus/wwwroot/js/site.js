e7c3a6bb2632e397bb1a66f3deae1a4929197f5c
Afa Cheng <afa@afa.moe>
2016-06-17 12:12:05 +0800

    Version 1.0.1 Shows user skin now Added LICENSE

--- src/MinecraftStatus/wwwroot/js/site.js
+++ src/MinecraftStatus/wwwroot/js/site.js
@@ -1,19 +1,43 @@
 ﻿// Write your Javascript code.
+$(document).mousemove(function(e) {
+    window.mouseX = e.pageX;
+    window.mouseY = e.pageY;
+});
+
 var playerSkinCache = new Map();
 
-function renderPlayerInfo(playerInfo) {
-    
+function renderPlayerSkin(hoverElement, skin) {
+//    console.log("Loaded");
+    var skinElement = $('<div class="skin-popup" />');
+    hoverElement.append(skinElement);
+    skinElement.css('left', mouseX);
+    skinElement.css('top', mouseY);
+    $skin = $(skin);
+    $skin.addClass('img-thumbnail');
+    $skin.appendTo(skinElement);
 }
 
-function getPlayerInfo(eventData) {
-    var playerName = eventData.playerName;
-    var skin = playerSkinCache.get(playerName);
+function showSkinPopup(element) {
+    var $element = $(element);
+    var uuid = $element.attr("player-id");
+    var skin = playerSkinCache.get(uuid);
 
-    if (playerInfo === undefined) {
-        $.ajax("http://crafatar.com/skins/" + playerName).done(function (data) {
-            playerSkinCache.set(playerName, data);
-        }).then(function () { renderPlayerInfo(skin); });
+    if (skin === undefined) {
+        img = new Image();
+        img.width = 120;
+        img.height = 270;
+        img.alt = $element.attr("player-name");
+        img.src = "http://mcapi.afa.moe/" + uuid;
+        img.onload = function () {
+            playerSkinCache.set(uuid, img);
+            renderPlayerSkin($element, img);
+        }
     } else {
-        renderPlayerInfo(skin);
+        renderPlayerSkin($element, skin);
     }
+}
+
+function removeSkinPopup(element) {
+    var $element = $(element);
+    $element.find('.skin-popup').remove();
 }
